deb7cfb49576fc2b647a125ac07aac03e525b79a
Vision Kim <junghwoan@gmail.com>
2026-09-23 03:51:57 +0900

    Set the DJ TY title in the condensed display face matched to Druk.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Instrument_Serif, Newsreader, Schibsted_Grotesk } from "next/font/google";
+import { Big_Shoulders, Newsreader, Schibsted_Grotesk } from "next/font/google";
 import { site } from "@/content/site";
 import "./globals.css";
 
@@ -10,9 +10,9 @@ const sans = Schibsted_Grotesk({
   display: "swap",
 });
 
-const display = Instrument_Serif({
+const display = Big_Shoulders({
   subsets: ["latin"],
-  weight: "400",
+  weight: ["600"],
   variable: "--font-display",
   display: "swap",
   adjustFontFallback: false,
